1a17b47d32f2cde5e940c6335fcf64bee158b648
relero90 <relero90@gmail.com>
2022-10-03 16:19:49 -0600

    added service worker, manifest file, css loader, and babel to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -17,10 +14,57 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-    plugins: [],
-
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+      // TODO: Add and configure workbox plugins for a service worker and manifest file.
+      new InjectManifest({
+        swSrc: "./src/sw.js",
+        swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E.",
+        description: "Takes notes with JavaScript syntax highlighting!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+        // https://www.npmjs.com/package/webpack-pwa-manifest
+        // icons: [
+        //   {
+        //     src: path.resolve("src/assets/icon.png"),
+        //     sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        //   },
+        //   {
+        //     src: path.resolve("src/assets/large-icon.png"),
+        //     size: "1024x1024", // you can also use the specifications pattern
+        //   },
+        //   {
+        //     src: path.resolve("src/assets/maskable-icon.png"),
+        //     size: "1024x1024",
+        //     purpose: "maskable",
+        //   },
+        // ],
+      }),
+    ],
+    // TODO: Add CSS loaders and babel to webpack.
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
     },
   };
 };
